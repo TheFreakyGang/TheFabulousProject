bacbe8ed2a3b33688859dda28e121edc3586468a
itsrealfortune <176779748+itsrealfortune@users.noreply.github.com>
2026-04-13 14:05:36 +0200

    feat: add tray support

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,30 @@
-#include <QGuiApplication>
+#include <QApplication>
+#include <QAction>
+#include <QIcon>
+#include <QMenu>
+#include <QSystemTrayIcon>
+#include <QStyle>
 #include <QQmlApplicationEngine>
 
 int main(int argc, char *argv[])
 {
-    QGuiApplication app(argc, argv);
+    QApplication app(argc, argv);
+    app.setApplicationName("The Fabulous Project");
+    app.setApplicationVersion("0.1");
+    app.setOrganizationName("The Fabulous Project");
+
+    if (QSystemTrayIcon::isSystemTrayAvailable()) {
+        QSystemTrayIcon trayIcon;
+        trayIcon.setIcon(QIcon::fromTheme("applications-graphics", app.style()->standardIcon(QStyle::SP_ComputerIcon)));
+        trayIcon.setToolTip("The Fabulous Project");
+
+        QMenu trayMenu;
+        QAction quitAction("Quitter", &app);
+        QObject::connect(&quitAction, &QAction::triggered, &app, &QCoreApplication::quit);
+        trayMenu.addAction(&quitAction);
+        trayIcon.setContextMenu(&trayMenu);
+        trayIcon.show();
+    }
 
     QQmlApplicationEngine engine;
     QObject::connect(
