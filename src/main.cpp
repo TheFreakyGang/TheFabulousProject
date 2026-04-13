5aea95b79dc2b9922a80eeeacebfd13886d22a33
Fortune <176779748+itsrealfortune@users.noreply.github.com>
2026-04-13 21:40:12 +0200

    feat: add tray support and basic app info
    Sqash of all the commits below:
    
    * feat: add tray support
    
    * style: auto-format C++ code with clang-format
    
    * feat: add Qt6::Widgets to CMakeLists.txt
    
    * feat: add CMAKE_PREFIX_PATH to CMake configuration step
    
    * feat: update org name
    
    * feat: refactor tray icon and menu initialization in main.cpp
    
    * fix: trayMenu instanciation fix
    
    ---------
    
    Co-authored-by: Clang Robot <robot@fabulous-project.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
-#include <QGuiApplication>
+#include <QAction>
+#include <QApplication>
+#include <QIcon>
+#include <QMenu>
 #include <QQmlApplicationEngine>
+#include <QStyle>
+#include <QSystemTrayIcon>
 
 int main(int argc, char *argv[])
 {
-    QGuiApplication app(argc, argv);
+    QApplication app(argc, argv);
+    app.setApplicationName("The Fabulous Project");
+    app.setApplicationVersion("0.1");
+    app.setOrganizationName("TheFreakyGang");
+
+    if (QSystemTrayIcon::isSystemTrayAvailable()) {
+        auto *trayIcon = new QSystemTrayIcon(&app);
+        trayIcon->setIcon(QIcon::fromTheme("applications-graphics",
+                                           app.style()->standardIcon(QStyle::SP_ComputerIcon)));
+        trayIcon->setToolTip("The Fabulous Project");
+
+        auto *trayMenu = new QMenu;
+        QObject::connect(&app, &QCoreApplication::aboutToQuit, trayMenu, &QObject::deleteLater);
+        QAction *quitAction = trayMenu->addAction(QObject::tr("Quit"));
+        QObject::connect(quitAction, &QAction::triggered, &app, &QCoreApplication::quit);
+        trayIcon->setContextMenu(trayMenu);
+        trayIcon->show();
+    }
 
     QQmlApplicationEngine engine;
     QObject::connect(
